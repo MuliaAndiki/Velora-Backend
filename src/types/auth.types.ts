014ac17113d1d609ea1cd162ac8a3e5371947138
diki <muliaandiki@gmaiil.com>
2025-09-30 21:10:34 +0700

    feat:Auth Types

--- src/types/auth.types.ts
+++ src/types/auth.types.ts
@@ -1,21 +1,25 @@
 import { Document } from "mongoose";
 
-export interface TAuth extends Document {
+export interface IAuth extends Document {
   _id: any;
   email: string;
   fullName: string;
   phone: string;
   password: string;
   token: string;
+  photoUrl?: string | null;
   createdAt: Date;
   updateAt: Date;
   __v: any;
 }
 
-export type JwtPayload = Pick<TAuth, "_id" | "email" | "fullName" | "phone">;
+export type JwtPayload = Pick<IAuth, "_id" | "email" | "fullName" | "phone">;
 export type PickRegister = Pick<
-  TAuth,
+  IAuth,
   "email" | "fullName" | "phone" | "password"
 >;
-export type PickLogin = Pick<TAuth, "email" | "password">;
-export type PickLogout = Pick<TAuth, "_id">;
+export type PickLogin = Pick<IAuth, "email" | "password">;
+export type PickLogout = Pick<IAuth, "_id">;
+
+export type PickEditProfile = Pick<IAuth, "fullName" | "email" | "phone">;
+export type PickGetProfile = Pick<IAuth, "_id">;
