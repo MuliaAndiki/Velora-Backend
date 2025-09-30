f6d0b6f2b97a7a15c9bb79443ccc5729a6012017
diki <muliaandiki@gmaiil.com>
2025-09-30 22:46:18 +0700

    fixed: vercel.json

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,8 @@
 import app from "./app";
 import connectDB from "./config/database";
 
-const PORT = process.env.PORT || 5000;
+// Connect to MongoDB
+connectDB();
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    console.log(`http://localhost:${PORT}`);
-  });
-});
+// Export the Express app for Vercel
+export default app;
